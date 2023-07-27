b6fe14c58ddcc141546f5b6bfebfa10720566f59
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-27 14:10:52 +0200

    add new idea

---
+++ list.c
@@ -0,0 +1,9 @@
+/* List structure: */
+/*      void **array_p */
+/*      int length */
+
+/* Append example: */
+/*      1. take pointer to value from user */
+/*	2. enlarge array_p by size of the pointer */
+/*	3. assign the pointer to array_p[length] */
+/*      4. increase length */
