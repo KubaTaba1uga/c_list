57987eedaadba912c93305c876e2374dfa6ecf9d
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-27 13:54:43 +0200

    add tests cleanup

--- tests/test_dl_body.c
+++ tests/test_dl_body.c
@@ -1,5 +1,6 @@
 #include <criterion/alloc.h>
 #include <criterion/criterion.h>
+#include <criterion/internal/assert.h>
 #include <criterion/parameterized.h>
 
 #include <stddef.h>
@@ -49,40 +50,73 @@ Test(set_dl_body, test_valid_value) {
             "Expected \"b.value\" to have address of \"value_b\".");
 }
 
+void dl_body_in_list_ctx_cleanup(struct criterion_test_params *crp) {
+  dl_body *b = crp->params;
+  dl_body *b_next = b->next, *b_prev = b->prev;
+
+  dl_body *b_to_clean[] = {b, b_next, b_prev};
+
+  int i;
+
+  for (i = 0; i < (int)(sizeof(b_to_clean) / sizeof(dl_body *)); i++) {
+    cr_free(b_to_clean[i]->value);
+    cr_free(b_to_clean[i]);
+  }
+}
+
 ParameterizedTestParameters(dl_body_in_list_ctx, test_getters) {
   /* Body chunk in list context has setted up attributes: */
   /*   - next */
   /*   - prev */
   /*   - value */
-  dl_body *b, *b_next, *p_body;
-  int *value_p;
-  int value = 5;
 
-  b = (dl_body *)cr_malloc(sizeof(dl_body));
-  b_next = (dl_body *)cr_malloc(sizeof(dl_body));
-  p_body = (dl_body *)cr_malloc(sizeof(dl_body));
-  value_p = (int *)cr_malloc(sizeof(int));
+  dl_body *b, *b_next, *b_prev;
+  int i;
 
-  init_dl_body(b, value_p);
-  init_dl_body(b_next, value_p);
-  init_dl_body(p_body, value_p);
+  dl_body **b_to_alloc[] = {&b, &b_next, &b_prev};
 
-  set_prev_dl_body(b, p_body);
-  set_next_dl_body(p_body, b);
+  for (i = 0; i < (int)(sizeof(b_to_alloc) / sizeof(dl_body *)); i++) {
+    dl_body *b_local;
+
+    *b_to_alloc[i] = b_local = (dl_body *)cr_malloc(sizeof(dl_body));
+
+    int *v_local = (int *)cr_malloc(sizeof(int));
+    *v_local = i;
+
+    init_dl_body(b_local, v_local);
+  }
+
+  set_prev_dl_body(b, b_prev);
+  set_next_dl_body(b_prev, b);
 
   set_prev_dl_body(b_next, b);
   set_next_dl_body(b, b_next);
 
-  *value_p = value;
-
-  return cr_make_param_array(dl_body, b, 1);
+  return cr_make_param_array(dl_body, b, 1, dl_body_in_list_ctx_cleanup);
 }
 
-#include <criterion/new/assert.h>
 ParameterizedTest(dl_body *b, dl_body_in_list_ctx, test_getters) {
-  dl_body *b_next, b_prev;
-  int value = 5;
+  dl_body *b_next, *b_prev;
+  int received_value;
 
-  /* cr_fatal("Parameter: %p", val->value); */
-  cr_fatal("Parameter: %i", *(int *)b->value);
+  b_next = get_next_dl_body(b);
+  cr_assert_not_null(b_next);
+
+  b_prev = get_prev_dl_body(b);
+  cr_assert_not_null(b_prev);
+
+  received_value = *(int *)get_value_dl_body(b);
+
+  cr_expect(received_value == 0,
+            "Expected \"*b->value\" to have value of \"0\".");
+
+  received_value = *(int *)get_value_dl_body(b_next);
+
+  cr_expect(received_value == 1,
+            "Expected \"*b_next->value\" to have value of \"1\".");
+
+  received_value = *(int *)get_value_dl_body(b_prev);
+
+  cr_expect(received_value == 2,
+            "Expected \"*b_prev->value\" to have value of \"2\".");
 }
