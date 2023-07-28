a177b3c9c59d8ff3222eb36085c6d6cb1acd137b
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-28 20:13:44 +0200

    fix bugs

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
+utils.o:  src/utils/memory.c src/utils/memory.h
+	gcc -Wall -c src/utils/memory.c -o utils.o
+
 ar_list.o: src/ar_list.c src/ar_list.h
 	gcc -Wall -c src/ar_list.c -o ar_list.o
 
 test_ar_list.o: tests/test_ar_list.c 
 	gcc -Wall -c tests/test_ar_list.c -o test_ar_list.o 
 
-test: ar_list.o test_ar_list.o
-	gcc -Wall -zmuldefs ar_list.o test_ar_list.o -l criterion -o test
+test: ar_list.o test_ar_list.o utils.o
+	gcc -Wall -zmuldefs ar_list.o test_ar_list.o utils.o -l criterion -o test
--- tests/test_ar_list.c
+++ tests/test_ar_list.c
@@ -7,13 +7,13 @@
 // including source file instead of header file allows
 //   for static functions testing.
 // Do not forget adding `-zmuldefs` to gcc.
-#include "../src/list.c"
+#include "../src/ar_list.c"
 
 Test(init_list, success) {
-  list l;
+  ar_list l;
   size_t ds = 255;
 
-  l_init_list(&l, ds);
+  arl_init(&l, ds);
 
   cr_expect_not_null(l.body);
   cr_expect(l.max_i == 0, "Expected \"l.length\" to have a value of 0.");
@@ -25,22 +25,22 @@ Test(count_new_size_list, 0and1000) {
   int len = 0;
   size_t size = 1000;
 
-  size = l_count_new_size(len, size);
+  size = arl_count_new_size(len, size);
   len = size;
 
   cr_assert(eq(ulong, (unsigned long int)size, (unsigned long int)1000));
 
-  size = l_count_new_size(len, size);
+  size = arl_count_new_size(len, size);
   len = size;
 
   cr_assert(eq(ulong, (unsigned long int)size, (unsigned long int)2500));
 
-  size = l_count_new_size(len, size);
+  size = arl_count_new_size(len, size);
   len = size;
 
   cr_assert(eq(ulong, (unsigned long int)size, (unsigned long int)6250));
 
-  size = l_count_new_size(len, size);
+  size = arl_count_new_size(len, size);
   len = size;
 
   cr_assert(eq(ulong, (unsigned long int)size, (unsigned long int)15625));
