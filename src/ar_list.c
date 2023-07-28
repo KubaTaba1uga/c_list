db7af8b3a42a64ee2499e9c687952625abe41288
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-28 20:36:06 +0200

    refactor

--- src/ar_list.c
+++ src/ar_list.c
@@ -5,34 +5,34 @@
 #include "ar_list.h"
 #include "utils/memory.h"
 
-static size_t arl_count_new_size(int max_i, size_t size) {
-  /* List's growth ratio. */
-  return (size_t)(3 * max_i / 2 + (int)size);
-}
-
-static size_t arl_count_new_size_with_pointer(int max_i, size_t size) {
-  return arl_count_new_size(max_i, size) * get_pointer_size();
-}
-
-void *arl_init(ar_list *l, size_t default_size) {
-  /* `default_size` tells maximum amount of pointers
+void *arl_init(ar_list *l, size_t default_capacity) {
+  /* `default_capacity` tells maximum amount of pointers
    *     which list can store until realloc. */
-  /*  `default_size` is parametrized for flexibility. */
+  /*  `default_capacity` is parametrized for flexibility. */
   /* User is responsible for knowing the data. */
 
-  void *list_body = arl_init_body(l, default_size);
+  void *arl_array = arl_init_array(l, default_capacity);
 
-  if (!list_body)
+  if (!arl_array)
     return NULL;
 
-  l->body = list_body;
-  l->size = default_size;
-  l->max_i = 0;
+  l->array = arl_array;
+  l->capacity = default_capacity;
+  l->size = 0;
 
   return l;
 }
 
-void *arl_init_body(ar_list *l, size_t default_size) {
+static void *arl_init_array(ar_list *l, size_t default_capacity) {
   /* return NULL if not enough memory. */
-  return malloc(arl_count_new_size_with_pointer(0, default_size));
+  return malloc(arl_count_new_capacity(0, default_capacity));
+}
+
+static size_t arl_count_new_capacity(int max_i, size_t size) {
+  return arl_count_new_capacity_base(max_i, size) * get_pointer_size();
+}
+
+static size_t arl_count_new_capacity_base(int max_i, size_t size) {
+  /* List's growth ratio. */
+  return (size_t)(3 * max_i / 2 + (int)size);
 }
--- src/ar_list.h
+++ src/ar_list.h
@@ -1,38 +1,21 @@
-/* List structure: */
-/*      void *array_p */
-/*      int length */
-
-/* Append example: */
-/*      1. take pointer to value from user */
-/*	2. enlarge array_p by size of the pointer */
-/*	3. assign the pointer to array_p[length] */
-/*      4. increase length */
-
-/* A lot of reallocs: */
-/*   To not do realloc on each append/insert/delete */
-/*   malloc for array_p and realloc only if size not enough */
-/*   or decreased significantly. */
-/* Idea came from here
- * https://stackoverflow.com/questions/19206005/array-of-structs-memory-realloc-stderr/19206121#19206121
- */
-
+/* Array list implementation, as described here: */
+/*   https://en.wikipedia.org/wiki/Dynamic_array   */
 #include <stddef.h>
 
 typedef struct {
 
-  /* `body` is an array of generic pointers. */
-  /* Values do not need to be of the same type. */
-  void *body;
+  void *array;
 
-  /* `max_i` is the list's iteration upper boundary. */
-  /* Useful when read from the list. */
-  int max_i;
-
-  /* `size` is maximum amount of pointers that */
-  /*   body can hold. */
-  /* Useful when write to the list. */
+  /* The number of elements.*/
   size_t size;
+
+  /* The size of the underlying array, */
+  /* which is the maximum possible size without relocating data. */
+  size_t capacity;
 } ar_list;
 
 extern void *arl_init(ar_list *l, size_t default_size);
-extern void *arl_init_body(ar_list *l, size_t default_size);
+
+static void *arl_init_array(ar_list *l, size_t default_size);
+static size_t arl_count_new_capacity(int max_i, size_t size);
+static size_t arl_count_new_capacity_base(int max_i, size_t size);
